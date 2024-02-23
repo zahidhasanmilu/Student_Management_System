b60905d9edac5eeb07513512d2d050eae46b3b67
Zahid Hasan Milu <minions.milu889@gmail.com>
2024-02-23 06:53:17 +0600

    header welcome username

--- app/models.py
+++ app/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    USER = (
-        (1, 'HOD'),
-        (2, 'STAFF'),
-        (3, 'STUDENT'),
+    USER_TYPE = (
+        ('1', 'HOD'),
+        ('2', 'STAFF'),
+        ('3', 'STUDENT'),
     )
 
-    user_type = models.CharField(choices=USER, max_length=50, default=1)
+    user_type = models.CharField(choices=USER_TYPE, max_length=50, default=1)
     profile_pic = models.ImageField(
         upload_to='Profile_Pics/', blank=True, null=True, max_length=None)
