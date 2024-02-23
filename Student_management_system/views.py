e0d0197907c4083f4cfb9f553d73194b82559786
Zahid Hasan Milu <minions.milu889@gmail.com>
2024-02-23 06:23:49 +0600

    base login logout  classbase with required permission

--- Student_management_system/views.py
+++ Student_management_system/views.py
@@ -1,57 +1,76 @@
-from django.shortcuts import render, HttpResponseRedirect,get_object_or_404, redirect, HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, redirect, HttpResponse
 from django.urls import reverse, reverse_lazy
 
 # VIEW
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, View, TemplateView, DeleteView
 
-#Authentication
+# Authentication
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 
-#forms
+# forms
 from django.contrib.auth.forms import AuthenticationForm
 
-#models
+# models
 # from django.contrib.auth.models import User
 
-#EmailBackEnd
+# Login MIXIN
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from .mixins import LogoutRequiredMixin
+
+# EmailBackEnd
 from app.EmailBackEnd import EmailBackEnd
 
-#message
+# message
 from django.contrib import messages
 
 import uuid
-#For Multiple fields Filter
+# For Multiple fields Filter
 from django.db.models import Q
 
-#create View Here  
-def BASE(request):
-    return render(request, 'base.html')
+# create View Here
 
 
-def LOGIN(request):
-    
-    return render(request, 'login.html')
+@method_decorator(never_cache, name='dispatch')
+class BASE(LoginRequiredMixin, TemplateView):
+    template_name = 'base.html'
 
-def doLogin(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = EmailBackEnd.authenticate(request, username=email, password=password)
-        
+
+@method_decorator(never_cache, name='dispatch')
+class LOGIN(LogoutRequiredMixin, View):
+    def get(self,  *args, **kwargs):
+        return render(self.request, 'login.html')
+
+
+@method_decorator(never_cache, name='dispatch')
+class doLogin(LogoutRequiredMixin, View):
+    def post(self,  *args, **kwargs):
+        email = self.request.POST.get('email')
+        password = self.request.POST.get('password')
+        user = EmailBackEnd.authenticate(
+            self.request, username=email, password=password)
+
         if user:
-            login(request, user)
+            login(self.request, user)
             user_type = user.user_type
             if user_type == '1':
-                return HttpResponse("This Is Head Of Department Panel")
+                return redirect('hod_home')
             elif user_type == '2':
                 return HttpResponse("This Is Staff Panel")
             elif user_type == '3':
                 return HttpResponse("This Is Student Panel")
             else:
-                messages.error(request, 'Email and password are invalid !')
+                messages.error(
+                    self.request, 'Email and password are invalid !')
                 return redirect('login')
         else:
-            messages.error(request, 'Email and password are invalid !')
+            messages.error(self.request, 'Email and password are invalid !')
             return redirect('login')
+
+
+def doLogout(request):
+    logout(request)
+    messages.success(request, 'Logout Successfully')
+    return redirect('login')
